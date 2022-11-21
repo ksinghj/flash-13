ed8d03d24adb2ec1d33391e418ca7b80b0da1b53
ksinghj <kartarsjabanda@gmail.com>
2022-11-21 20:35:21 +0000

    feat(css): adds tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Flash</h1>
+      <h1 className='text-3xl font-bold underline'>Flash</h1>
     </div>
   )
 }
