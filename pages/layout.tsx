af2640a2ff7ce76e3211518e1542425ddcd0769f
ksinghj <kartarsjabanda@gmail.com>
2022-11-21 21:48:08 +0000

    feat(app): adds assets

---
+++ pages/layout.tsx
@@ -0,0 +1,11 @@
+export default function HomeLayout() {
+  return (
+    <nav>
+      <div>Logo</div>
+      <div>Home</div>
+      <div>Create Deck</div>
+      <div>Settings</div>
+      <div>User Image</div>
+    </nav>
+  )
+}
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+// prettier.config.js
+module.exports = {
+    plugins: [require('prettier-plugin-tailwindcss')],
+    tailwindConfig: './tailwind.config.js',
+    tabWidth: 2,
+    semi: false,
+    singleQuote: true
+}
